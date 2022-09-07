a97dbc9978638a77bcc724148ff6685d4e7f10a6
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-09-07 20:36:19 +0300

    Update ProductsController.php

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
     
 use App\Models\Products;    
-use App\Models\sections; 
+use App\Models\sections;  
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
