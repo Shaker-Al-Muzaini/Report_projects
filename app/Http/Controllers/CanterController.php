cee5217753a699848fa9530739a3633b946b654d
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-09-13 19:43:45 +0300

    Update CanterController.php

--- app/Http/Controllers/CanterController.php
+++ app/Http/Controllers/CanterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\canter;
 use Illuminate\Http\Request;
 
-class CanterController extends Controller
+class CanterController extends Controller 
 {
     /**
      * Display a listing of the resource.
